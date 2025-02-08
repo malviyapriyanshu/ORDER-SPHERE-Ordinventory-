ad754ad4644d6b835fea448cdac5c847ba99a903
Priyanshu Malviya <priyanshumalviya64@gmail.com>
2025-02-08 22:01:26 +0530

    feat: Add detailed comments and documentation for Category model and DTO

--- Ordinventory/Models/Category.cs
+++ Ordinventory/Models/Category.cs
@@ -10,3 +10,27 @@ namespace Ordinventory.Models
         public int DisplayOrder { get; set; }
     }
 }
+
+
+/*
+
+    Registor:- Id(Self Generated) (Unique) (Confidential), name, email, password
+
+    Category :- name, DisplayOrder (Category API)-------> {
+                                                            (Hide) Id,-->(Error)
+                                                            Name,
+                                                            DisplayOrder
+                                                        }
+
+    CategoryDTO :- Name, DisplayOrder (CategoryDTO API)-------> {
+                                                            Name,
+                                                            DisplayOrder
+                                                        }
+    Category :- Id, name, DisplayOrder
+
+
+    CategoryDTO :- 
+        category.Name = Name, category.DisplayOrder = DisplayOrder
+
+        return Ok(categoryDTO)
+*/
